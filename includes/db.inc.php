5831bf0277dc7b4517dd24cb18ed9de2cc7e2760
Alyda Hultstrand <Alyda.Hultstrand@my.ndsu.edu>
2011-10-31 12:55:02 -0500

    first commit

---
+++ includes/db.inc.php
@@ -0,0 +1,43 @@
+<?php
+
+$link = mysqli_connect('localhost', 'root', 'Smile :)');
+
+
+if (!$link) {
+
+	$error = 'Unable to connect to the database server.';
+
+	include 'error.html.php';
+
+	exit();
+
+}
+
+
+
+
+if (!mysqli_set_charset($link, 'utf8')) {
+
+	$output = 'Unable to set database connection encoding.';
+
+	include 'output.html.php';
+
+	exit();
+
+}
+
+
+
+
+if (!mysqli_select_db($link, 'ijdb')) {
+	$output = 'Unable to locate the joke database.';
+	include 'error.html.php';
+
+	exit();
+
+}
+
+
+
+
+?>
